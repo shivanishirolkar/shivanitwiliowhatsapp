abe02cc124295f3d362f51908489c7ba819e1265
Shivani Shirolkar <snshirol@ncsu.edu>
2022-01-20 21:47:47 -0500

    spelling error

--- messages/settings.py
+++ messages/settings.py
@@ -15,7 +15,7 @@ import os
 
 OPEN_WEATHER_API_KEY = os.getenv("OPEN_WEATHER_API_KEY")
 TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
-TWILIO_AUTH_TOKEN = os.getenv("WILIO_AUTH_TOKEN")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
